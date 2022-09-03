d60c0d887b385166b99787617e73221927f79c26
Aspect <32452006+aspectdev-ph@users.noreply.github.com>
2022-09-03 08:13:04 +0800

    Update Files
    Tailwindcss
    Notif handler
    ui events
    record management UI

--- assets/js/notif_handler.js
+++ assets/js/notif_handler.js
@@ -1,5 +1,16 @@
-const notif=new Notyf();
-
-function error(message){
-    
-}
+var notif = new Notyf({
+    duration: 5000,
+    ripple: true,
+    position: {
+      x: "right",
+      y: "top",
+    },
+    dismissible: true,
+  });
+  function success(message) {
+    notif.success(message);
+  }
+  function error(message) {
+    notif.error(message);
+  }
+  
--- assets/js/ui_events.js
+++ assets/js/ui_events.js
@@ -1,14 +1,36 @@
-$("#close_record_detail").on("click",(event)=>{
+$("#close_record_detail").on("click", (event) => {
     event.preventDefault()
     $("#view_record_details").addClass("hidden")
 })
-$(".table_row_record_details").on("click",(event)=>{
+$(".table_row_record_details").on("click", (event) => {
     event.preventDefault()
+    console.log(event)
+    if (event.target.id.includes("edit") || event.target.id.includes("delete")) {
+        return 0;
+    }
     $("#view_record_details").removeClass("hidden")
 })
-$("#new_operation").on("click",()=>{
-
+$("#cancel_add_record_detail").on("click", () => {
+    $("#add_record_details").addClass("hidden")
 })
-$("#operationdate").on("click",()=>{
-    $("#operation_date").showPicker();
+$("#new_operation").on("click", () => {
+    $("#add_record_details").removeClass("hidden")
+})
+
+function edit_operation_details(id) {
+    $("#edit_record_details").removeClass("hidden")
+}
+$("#cancel_edit_record_detail").on("click", () => {
+    $("#edit_record_details").addClass("hidden")
+})
+function remove_operation_details(id) {
+    $("#remove_record_details").removeClass("hidden")
+}
+$("#cancel_remove_record_detail").on("click", () => {
+    $("#remove_record_details").addClass("hidden")
+})
+$("#confirm_add_record_details").on("click", (event) => {
+    success("New operation details has been stored.")
+    $("#add_record_details").addClass("hidden")
+
 })
